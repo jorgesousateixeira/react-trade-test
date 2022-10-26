445e9edff74464f0a53d3396192049ad9431e2f0
Jorge Teixeira <jorge.teixeira@sovos.com>
2022-10-26 11:41:19 +0100

    login - WIP

--- src/public/login/login.tsx
+++ src/public/login/login.tsx
@@ -42,54 +42,58 @@ export function Login () {
 
     return (
         <>
-            <div className={styles.toolbar}>
-                <Link to="/public">
-                <a>{t('loginPage.backToPublicHome')}</a>
-            </Link>
-                <div>
-                    {Object.values(LanguagesEnum).map((lng) => (
-                        <Button key={lng} style={
-                            {fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal'}}
-                                type="submit"
-                                onClick={() => i18n.changeLanguage(lng)}>
-                            {lng}
-                        </Button>
-                    ))}
-                </div>
-            </div>
             {/* animation test */}
             <Fade>
-                <div className={styles.loginContainer}>
-                <div className={styles.loginFormTitle}>
-                    <span className={styles.appName}>Trade</span>
-                    <span className={styles.appName2}>Admin</span>
+                <div className={styles.loginOuterContainer}>
+                    <div className={styles.loginFormContainer}>
+                    <div className={styles.loginFormTitle}>
+                        <span className={styles.appName}>Trade</span>
+                        <span className={styles.appName2}>Admin</span>
+                    </div>
+                    <div className={styles.enterCredentials}>{t('loginPage.enterCredentials')}</div>
+                    <Box className={styles.loginBox}>
+                        <form onSubmit={handleLogin}>
+                            <TextField id="username"
+                                       className={styles.loginData}
+                                       label={t('loginPage.userName')}
+                                       variant="standard"
+                                       value={username}
+                                       fullWidth
+                                       onChange={(e) => setUsername(e.target.value)}/>
+                            <TextField id="password"
+                                       className={styles.loginData}
+                                       label="Password"
+                                       variant="standard"
+                                       type="password"
+                                       fullWidth
+                                       value={password}
+                                       onChange={(e) => setPassword(e.target.value)}/>
+                            <Button className={styles.loginBtn}
+                                    variant="contained" type={"submit"}>
+                                {t('loginPage.btnLogin')}
+                            </Button>
+                        </form>
+                    </Box></div>
+                    <div className={styles.loginImage}>
+                        <div className={styles.toolbar}>
+                            <Link to="/public">
+                                <a>{t('loginPage.backToPublicHome')}</a>
+                            </Link>
+                            <div>
+                                {Object.values(LanguagesEnum).map((lng) => (
+                                    <Button key={lng} style={
+                                        {fontWeight: i18n.resolvedLanguage === lng ? 'bold' : 'normal'}}
+                                            type="submit"
+                                            onClick={() => i18n.changeLanguage(lng)}>
+                                        {lng}
+                                    </Button>
+                                ))}
+                            </div>
+                        </div>
+                        <div className={styles.loginTextOverImage}> SOME TEXT OVER IMAGE</div>
+                    </div>
                 </div>
-                <div className={styles.enterCredentials}>{t('loginPage.enterCredentials')}</div>
-                <Box className={styles.loginBox}>
-                    <form onSubmit={handleLogin}>
-                        <TextField id="username"
-                                   className={styles.loginData}
-                                   label={t('loginPage.userName')}
-                                   variant="standard"
-                                   value={username}
-                                   fullWidth
-                                   onChange={(e) => setUsername(e.target.value)}/>
-                        <TextField id="password"
-                                   className={styles.loginData}
-                                   label="Password"
-                                   variant="standard"
-                                   type="password"
-                                   fullWidth
-                                   value={password}
-                                   onChange={(e) => setPassword(e.target.value)}/>
-                        <Button className={styles.loginBtn}
-                                variant="contained" type={"submit"}>
-                            {t('loginPage.btnLogin')}
-                        </Button>
-                    </form>
-                </Box></div>
             </Fade>
-
         </>
     );
 }
